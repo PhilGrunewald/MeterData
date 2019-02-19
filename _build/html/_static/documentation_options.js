ea8d3da733b345a3331ed461e3e58e32886bd878
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2019-02-19 10:39:56 +0000

    ready for deposition

--- _build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: 'ASAP',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
